36a01958c82b192a1adac9250b5953459326b536
AlifZulhidwalHsb <122459234+AlifZulhidwalHsb@users.noreply.github.com>
2024-10-15 11:16:03 +0700

    Add files via upload

---
+++ Coba/server.py
@@ -0,0 +1,19 @@
+import grpc
+import greet_pb2
+import greet_pb2_grpc
+
+import concurrent.futures  # Import for Python versions <= 3.5
+
+class GreeterServicer(greet_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        return greet_pb2.HelloReply(message=f"Hello, {request.name}!")
+
+def serve():
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))  # Use concurrent.futures for Python <= 3.5
+    greet_pb2_grpc.add_GreeterServicer_to_server(GreeterServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
